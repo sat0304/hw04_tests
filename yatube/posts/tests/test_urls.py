98a37acb104d71c8a8bff17ebdf1acdbf5be7ba4
Серегей Токарев <sat0304@yandex.ru>
2021-12-13 23:24:48 +0400

    test urls has started a little

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,14 +1,44 @@
+from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 
+from models import Group, Post
 
-class StaticURLTests(TestCase):
+
+User = get_user_model()
+
+class PostURLTests(TestCase):
     """ Тест страниц сайта."""
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='auth')
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='Тестовый слаг',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            author=cls.user,
+            text='Тестовый_текст',
+        )
 
     def setUp(self):
         """Клиент неавторизован."""
         self.guest_client = Client()
+        """Клиент авторизован."""
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
 
-    def test_homepage(self):
-        response = self.guest_client.get('/')
-        self.assertEqual(response.status_code, 200)
-
+    
+    def test_urls_uses_correct_template(self):
+        """URL-адрес использует соответствующий шаблон."""
+        templates_url_names = {
+            'posts/index.html': '/',
+            'group_list.html': '/added/',
+            'deals/task_list.html': '/task/',
+            'deals/task_detail.html': '/task/test-slug/',
+        }
+        for template, adress in templates_url_names.items():
+            with self.subTest(adress=adress):
+                response = self.authorized_client.get(adress)
+                self.assertTemplateUsed(response, template)
