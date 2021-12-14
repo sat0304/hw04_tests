6e2a98702c7cee21016c0ed4022c452a251a720c
Сергей Токарев <sat0304@yandex.ru>
2021-12-14 17:45:40 +0400

    view test for templates was added

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,24 +1,25 @@
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 
-from models import Group, Post
-
+from posts.models import Group, Post
 
 User = get_user_model()
+
 
 class PostURLTests(TestCase):
     """ Тест страниц сайта."""
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.user = User.objects.create_user(username='auth')
+        cls.user = User.objects.create_user(username='test_user')
+        cls.auth = User.objects.create_user(username='auth')
         cls.group = Group.objects.create(
             title='Тестовая группа',
-            slug='Тестовый слаг',
+            slug='test_slug',
             description='Тестовое описание',
         )
         cls.post = Post.objects.create(
-            author=cls.user,
+            author=cls.auth,
             text='Тестовый_текст',
         )
 
@@ -28,15 +29,66 @@ class PostURLTests(TestCase):
         """Клиент авторизован."""
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
+        self.authorized_author = Client()
+        self.authorized_author.force_login(self.auth)
 
-    
+    def test_url_exists_at_desired_location_auth(self):
+        """URL-адрес существует для автора поста."""
+        templates_url_names = {
+            '/': 200,
+            f'/group/{self.group.slug}/': 200,
+            f'/profile/{self.post.author}/': 200,
+            f'/posts/{self.post.pk}/': 200,
+            f'/posts/{self.post.pk}/edit/': 200,
+            '/create/': 200,
+            'unexist.html': 404,
+        }
+        for template, status in templates_url_names.items():
+            with self.subTest(status=status):
+                response = self.authorized_author.get(template)
+                self.assertEqual(response.status_code, status)
+
+    def test_url_exists_at_desired_location_user(self):
+        """URL-адрес существует для зарегистрированного."""
+        templates_url_names = {
+            '/': 200,
+            f'/group/{self.group.slug}/': 200,
+            f'/profile/{self.post.author}/': 200,
+            f'/posts/{self.post.pk}/': 200,
+            f'/posts/{self.post.pk}/edit/': 302,
+            '/create/': 200,
+            'unexist.html': 404,
+        }
+        for template, status in templates_url_names.items():
+            with self.subTest(status=status):
+                response = self.authorized_client.get(template)
+                self.assertEqual(response.status_code, status)
+
+    def test_url_exists_at_desired_location_not_auth(self):
+        """URL-адрес существует для незарегистрированного."""
+        templates_url_names = {
+            '/': 200,
+            f'/group/{self.group.slug}/': 200,
+            f'/profile/{self.post.author}/': 200,
+            f'/posts/{self.post.pk}/': 200,
+            f'/posts/{self.post.pk}/edit/': 302,
+            '/create/': 302,
+            'unexist.html': 404,
+        }
+        for template, status in templates_url_names.items():
+            with self.subTest(status=status):
+                response = self.guest_client.get(template)
+                self.assertEqual(response.status_code, status)
+
     def test_urls_uses_correct_template(self):
         """URL-адрес использует соответствующий шаблон."""
         templates_url_names = {
             'posts/index.html': '/',
-            'group_list.html': '/added/',
-            'deals/task_list.html': '/task/',
-            'deals/task_detail.html': '/task/test-slug/',
+            'posts/group_list.html': f'/group/{self.group.slug}/',
+            'posts/profile.html': f'/profile/{self.post.author}/',
+            'posts/post_detail.html': f'/posts/{self.post.pk}/',
+            'posts/create_post.html': f'/posts/{self.post.pk}/edit/',
+            'posts/create_post.html': '/create/',
         }
         for template, adress in templates_url_names.items():
             with self.subTest(adress=adress):
---
+++ yatube/posts/tests/test_views.py
@@ -0,0 +1,108 @@
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+from django import forms
+
+from posts.models import Group, Post
+
+User = get_user_model()
+
+
+class PostPagesTests(TestCase):
+    """ Тест страниц сайта."""
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='test_user')
+        cls.auth = User.objects.create_user(username='auth')
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='test_slug',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            author=cls.auth,
+            text='Тестовый_текст',
+        )
+
+    def setUp(self):
+        self.guest_client = Client()
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
+
+    def test_pages_uses_correct_template(self):
+        """URL-адрес использует соответствующий шаблон."""
+        templates_page_names = {
+            'posts/index.html': reverse('posts:index'),
+            'posts/group_list.html': reverse('posts:group_list'),
+            'posts/profile.html': reverse('posts:profile'),
+            'posts/post_detail.html': reverse('posts:post_detail'),
+            'posts/create_post.html': reverse('posts:post_edit'),
+            'posts/create_post.html': reverse('posts:post_create'),
+            #'deals/task_detail.html': (
+            #    reverse('deals:task_detail', kwargs={'slug': 'test-slug'})),
+        }
+        # Проверяем, что при обращении к name
+        # вызывается соответствующий HTML-шаблон
+        for template, reverse_name in templates_page_names.items():
+            with self.subTest(template=template):
+                response = self.authorized_client.get(reverse_name)
+                self.assertTemplateUsed(response, template)
+
+    def test_home_page_show_correct_context(self):
+        """Шаблон home сформирован с правильным контекстом."""
+        response = self.guest_client.get(reverse('posts:index'))
+        # Словарь ожидаемых типов полей формы:
+        # указываем, объектами какого класса должны быть поля формы
+        form_fields = {
+            'text': forms.fields.CharField,
+            'group': forms.fields.ChiceField,
+        }
+
+        # Проверяем, что типы полей формы в словаре context
+        # соответствуют ожиданиям
+        for value, expected in form_fields.items():
+            with self.subTest(value=value):
+                form_field = response.context['form'].fields[value]
+                # Проверяет, что поле формы является экземпляром
+                # указанного класса
+                self.assertIsInstance(form_field, expected)
+
+    def test_task_list_page_list_is_1(self):
+        # Удостоверимся, что на страницу со списком заданий передаётся
+        # ожидаемое количество объектов
+        
+        response = self.authorized_client.get(reverse('deals:task_list'))
+        self.assertEqual(response.context['object_list'].count(), 1)
+
+    # Проверяем, что словарь context страницы /task
+    # в первом элементе списка object_list содержит ожидаемые значения
+    def test_task_list_page_show_correct_context(self):
+        """Шаблон task_list сформирован с правильным контекстом."""
+        response = self.authorized_client.get(reverse('deals:task_list'))
+        # Взяли первый элемент из списка и проверили, что его содержание
+        # совпадает с ожидаемым
+        first_object = response.context['object_list'][0]
+        task_title_0 = first_object.title
+        task_text_0 = first_object.text
+        task_slug_0 = first_object.slug
+        self.assertEqual(task_title_0, 'Заголовок')
+        self.assertEqual(task_text_0, 'Текст')
+        self.assertEqual(task_slug_0, 'test-slug')
+
+    # Проверяем, что словарь context страницы task/test-slug
+    # содержит ожидаемые значения
+    def test_task_detail_pages_show_correct_context(self):
+        """Шаблон task_detail сформирован с правильным контекстом."""
+        response = self.authorized_client.get(
+            reverse('deals:task_detail', kwargs={'slug': 'test-slug'})
+            )
+        self.assertEqual(response.context['task'].title, 'Заголовок')
+        self.assertEqual(response.context['task'].text, 'Текст')
+        self.assertEqual(response.context['task'].slug, 'test-slug')
+
+    def test_initial_value(self):
+        """Предустановленнное значение формы."""
+        response = self.guest_client.get(reverse('deals:home'))
+        title_inital = response.context['form'].fields['title'].initial
+        self.assertEqual(title_inital, 'Значение по-умолчанию')
