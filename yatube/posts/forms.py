37f9466302dcceb34fdebd0a4b5061cf31a8603f
Сергей Токарев <sat0304@yandex.ru>
2021-12-20 16:44:11 +0400

    HTTP response was added

---
+++ yatube/posts/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('text', 'group')
+        help_texts = {
+            'text': 'Здесь Вы можете напечатать все, что Вы думаете об этом',
+            'group': 'Выберете название сообщества для публикации',
+        }
---
+++ yatube/posts/tests/test_models.py
@@ -0,0 +1,66 @@
+#from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from ..models import Group, Post, User
+
+#User = get_user_model()
+
+
+class PostModelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='auth')
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='Тестовый слаг',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            author=cls.user,
+            text='Тестовый_текст',
+        )
+
+    def test_models_have_correct_object_names(self):
+        """Проверяем, что у моделей корректно работает __str__."""
+        post = PostModelTest.post
+        group = PostModelTest.group
+        field_object_names = {
+            group.title: 'Тестовая группа',
+            post.text: 'Тестовый_текст'
+        }
+        for field, expected_value in field_object_names.items():
+            with self.subTest(field=field):
+                self.assertEqual(
+                    str(field),
+                    expected_value
+                )
+
+    def test_model_have_correct_verbose_name(self):
+        """Проверяем, что у модели Post корректно работает verbose name."""
+        post = PostModelTest.post
+        field_verbose_names = {
+            'text': 'Текст поста',
+            'pub_date': 'Дата публикации',
+            'author': 'Автор',
+            'group': 'Группа',
+        }
+        for field, expected_value in field_verbose_names.items():
+            with self.subTest(field=field):
+                self.assertEqual(
+                    post._meta.get_field(field).verbose_name,
+                    expected_value
+                )
+
+    def test_model_have_correct_help_text(self):
+        """Проверяем, что у модели Post корректно работает help text."""
+        post = PostModelTest.post
+        field_help_texts = {
+            'text': 'Введите текст поста',
+            'group': 'Выберите группу',
+        }
+        for field, expected_value in field_help_texts.items():
+            with self.subTest(field=field):
+                self.assertEqual(
+                    post._meta.get_field(field).help_text, expected_value
+                )
